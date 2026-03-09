367bc13abb2735b9ca60d884b4659c09fb599562
DESKTOP-0V0B8D7\matsuoka <matsuokagame@gmail.com>
2026-03-10 04:02:13 +0900

    project push

---
+++ DeskUtils/FileDeleter/FileDeleter.cs
@@ -0,0 +1,85 @@
+﻿namespace FileDeleter
+{
+    /// <summary>
+    /// ファイルデリートクラス
+    /// </summary>
+    internal class FileDeleter
+    {
+        /// <summary>
+        /// 削除対象のファイルを確認
+        /// </summary>
+        /// <param name="targetPath">ターゲットパス</param>
+        /// <param name="keepPatterns">削除対象にしないファイル名パターン</param>
+        public void ReadDeleteFiles(string targetPath, string[]? keepPatterns)
+        {
+            if (!IsPathExists(targetPath))
+            {
+                Console.WriteLine("targetPath folder is not Exists");
+                return;
+            }
+            var fileNames = Directory.EnumerateFiles(targetPath).ToList();
+            if (fileNames == null || fileNames.Count == 0)
+            {
+                return;
+            }
+            Console.WriteLine("========== DeleteFiles ==========");
+            foreach (var fileName in fileNames)
+            {
+                if (keepPatterns == null || keepPatterns.Length ==0)
+                {
+                    Console.WriteLine(fileName);
+                    continue;
+                }
+
+                foreach(var pattern in keepPatterns)
+                {
+                    if (fileName.Contains(pattern))
+                    {
+                        continue;
+                    }
+                }
+                Console.WriteLine(fileName);
+            }
+            Console.WriteLine("========== End ==========");
+        }
+
+        /// <summary>
+        /// ファイル削除実行
+        /// </summary>
+        /// <param name="targetPath">ターゲットパス</param>
+        /// <param name="keepPatterns">削除対象にしないファイル名パターン</param>
+        public void ExecuteFileDelete(string targetPath, string[] keepPatterns)
+        {
+            if (!IsPathExists(targetPath))
+            {
+                Console.WriteLine("targetPath folder is not Exists");
+                return;
+            }
+            var fileNames = Directory.EnumerateFiles(targetPath);
+            if (fileNames == null || fileNames.Count() == 0)
+            {
+                return;
+            }
+            Console.WriteLine("========== DeleteFiles ==========");
+            foreach (var fileName in fileNames)
+            {
+                Console.WriteLine(fileName);
+            }
+            Console.WriteLine("========== End ==========");
+        }
+
+        /// <summary>
+        /// パスの存在有無をチェックします。
+        /// </summary>
+        /// <param name="targetPath">ターゲットパス</param>
+        /// <returns>true:存在する</returns>
+        private bool IsPathExists(string targetPath)
+        {
+            if (string.IsNullOrEmpty(targetPath))
+            {
+                return false;
+            }
+            return Directory.Exists(targetPath);
+        }
+    }
+}
---
+++ DeskUtils/FileDeleter/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Please select a file path. (copy and paste is allowed");
+
+// 入力待ち
+var input = Console.ReadLine();
+
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("finish...");
+    return;
+}
+// パスコピーで自動でつくダブルクォーテーションは排除
+var targetFile = input.Trim().Trim('"');
+
+Console.WriteLine("Please Keep Pattern ? , ");
+input = Console.ReadLine();
+
+string[]? keepPatterns = null;
+if (!string.IsNullOrEmpty(input))
+{
+    keepPatterns = input.Split(',');
+}
+
+var fileDeleter = new FileDeleter.FileDeleter();
+fileDeleter.ReadDeleteFiles(targetFile, keepPatterns);
